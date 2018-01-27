ff151cc8f2a1d5d00cac7677b7e0068484d1f2e7
Bruno Grossniklaus <grossnik@tuvok>
2018-01-27 11:56:43 +0100

    publish

--- docs/examples/functions/_create.sh
+++ docs/examples/functions/_create.sh
@@ -129,20 +129,25 @@ fi
 
 MY_WEIGHT=100
 for MY_DEMO in ${THE_DEMOS}; do
+  MY_DEMO_SMALL=$(echo ${MY_DEMO} | tr [A-Z] [a-z])  
   echo ${MY_DEMO}
+  if [ ! -e "${MY_DEMO}.md" ]; then
   cat > "${MY_DEMO}.md" <<EOF
 ---
 layout: "${MY_PATH}/${MY_DEMO}"
 weight: ${MY_WEIGHT}
 ---
 EOF
+  fi
+
 
   if [ ! -d "${MY_LAYOUT_PATH}" ]; then mkdir -p "${MY_LAYOUT_PATH}"; fi
   if [ ! -e "${MY_LAYOUT_PATH}/${MY_DEMO}.html" ]; then
     echo "${MY_LAYOUT_PATH}/${MY_DEMO}.html"
-  cat > "${MY_LAYOUT_PATH}/${MY_DEMO}.html" <<EOF
+  cat  > "${MY_LAYOUT_PATH}/${MY_DEMO}.html" <<EOF
 {{/* __HIDE__ */}} {{- define "${MY_BLOCK}" }}
 {{/* ${MY_DEMO} */}}
+{{"https://gohugo.io/functions/${MY_DEMO_SMALL}" | markdownify }}
 
 {{/* __HIDE__ */}} {{- end }}
 EOF
